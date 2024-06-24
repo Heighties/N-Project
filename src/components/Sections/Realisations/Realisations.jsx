167cc32c9e6f17d96ce66e8906135bcf73be5800
Heighties <heighties@gmail.com>
2024-06-24 20:42:00 +0200

    first deploy

--- src/components/Sections/Realisations/Realisations.jsx
+++ src/components/Sections/Realisations/Realisations.jsx
@@ -4,9 +4,9 @@ import data from '../../../datas/data';
 
 const Realisations = () => {
   return (
-    <div className="flex flex-col gap-4 w-full">
-      <div className="w-1/2 bg-black h-2 left-0 right-0 z-10"></div>
-      <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col justify-center gap-4 w-full min-h-screen xl:min-h-full">
+      <div className="w-1/2 top-0 bg-black h-2 left-0 right-0 z-10"></div>
+      <div className="flex flex-col min-h-full items-center justify-center">
         <div className="items-center">
           <Carrousel items={data}/>
         </div>
