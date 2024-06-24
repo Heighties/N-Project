88435b1fd4bbe2a4a60236411f4b03931618ed71
Heighties <heighties@gmail.com>
2024-06-24 17:10:59 +0200

    test deploy

--- src/components/Sections/Infos/Infos.jsx
+++ src/components/Sections/Infos/Infos.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 const Infos = () => {
   return (
     <div className="w-full flex p-4">
-      <div className="flex sm:flex-col md:flex-row sm:items-center md:items-center lg:items-stretch sm:gap-12 md:gap-12 lg:gap-0 w-full">
+      <div className="flex sm:flex-col md:flex-col lg:flex-col xl:flex-row sm:items-center md:items-center lg:it ems-center xl:items-stretch sm:gap-12 md:gap-12 lg:gap-12 xl:gap-0 w-full">
         <div className='md:w-1/2'>
-        <img src={process.env.PUBLIC_URL + "/photoP2.jpg"} alt="Profile" className="lg:w-full lg:h-full sm:w-96 sm:h-96 md:w-96 md:h-96 object-cover" />
+        <img src={process.env.PUBLIC_URL + "/photoP2.jpg"} alt="Profile" className="sm:w-96 sm:h-96 md:w-96 md:h-96 lg:w-96 lg:h-96 xl:w-full xl:h-full object-cover" />
         </div>
         <div className="flex flex-col gap-2 w-full">
           <h1 className="font-bold text-center sm:text-6xl md:text-4xl">
             <span>NIMA </span>
             <span>GHASHGHAEI</span>
           </h1>
-          <div className="flex flex-col gap-4 items-center flex-grow p-4 md:p-0">
+          <div className="flex flex-col gap-4 items-center flex-grow p-4 xl:p-0">
             <div className="w-full h-1 bg-black"></div>
-            <p className="text-justify ml-4 text-xs sm:text-2xl md:text-base lg:text-sm xl:text-md 2xl:text-md">
+            <p className="text-justify ml-4 text-xs sm:text-2xl md:text-base xl:text-sm xl:text-md 2xl:text-md">
             Je m'appelle Nima GHASHGHAEI, et je suis vidéaste passionné. Fort d'une formation spécialisée dans le domaine du documentaire, j'ai eu la chance de travailler sur une grande variété de projets et de toucher à différents genres cinématographiques. Mon parcours m'a permis d'explorer des domaines aussi divers que le film institutionnel, les teasers, le film documentaire, et même la fiction.  Ce qui m'anime avant tout, c'est la passion pour la vidéo et la création visuelle. Chaque projet est une nouvelle aventure où je m'engage pleinement pour garantir un résultat à la hauteur de vos attentes. Que vous ayez besoin d'un film institutionnel pour valoriser votre entreprise, d'un teaser percutant pour promouvoir un événement. Mon objectif est de donner vie à vos idées, de capturer et de transmettre l'émotion à travers l'image. Avec un œil attentif pour les détails et un engagement sans faille, je vous propose des réalisations de qualité qui sauront marquer les esprits.
 Merci de votre visite et au plaisir de collaborer sur vos futurs projets vidéo !
             </p>
