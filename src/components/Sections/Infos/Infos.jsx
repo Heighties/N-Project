a42ef6107dceb2fae55a0aa0bceaccdd2c12e2ec
Heighties <heighties@gmail.com>
2024-07-02 12:29:44 +0200

    responsive

--- src/components/Sections/Infos/Infos.jsx
+++ src/components/Sections/Infos/Infos.jsx
@@ -5,7 +5,7 @@ const Infos = () => {
     <div className="w-full flex p-4">
       <div className="flex flex-col md:flex-row items-center sm:items-center md:items-stretch sm:gap-12 md:gap-0 w-full">
         <div className='md:w-1/2'>
-        <img src={process.env.PUBLIC_URL + "/photoP2.jpg"} alt="Profile" className="w-40 h-40 md:w-full md:h-full object-cover object-top xl:object-center" />
+        <img src={process.env.PUBLIC_URL + "/photoP2.jpg"} alt="Profile" className="w-40 h-40 xl:w-full xl:h-full object-cover object-top xl:object-center" />
         </div>
         <div className="flex flex-col gap-2 w-full">
           <h1 className="font-bold text-center sm:text-4xl md:text-4xl">
@@ -14,7 +14,7 @@ const Infos = () => {
           </h1>
           <div className="flex flex-col gap-4 items-center flex-grow p-4 xl:p-0">
             <div className="w-full h-1 bg-black"></div>
-            <p className="text-justify ml-4 text-xs sm:text-2xl md:text-base xl:text-sm xl:text-md 2xl:text-md">
+            <p className="text-justify ml-4 text-xs md:text-xs xl:text-md 2xl:text-md">
             Je m'appelle Nima GHASHGHAEI, et je suis vidéaste passionné. Fort d'une formation spécialisée dans le domaine du documentaire, j'ai eu la chance de travailler sur une grande variété de projets et de toucher à différents genres cinématographiques. Mon parcours m'a permis d'explorer des domaines aussi divers que le film institutionnel, les teasers, le film documentaire, et même la fiction.  Ce qui m'anime avant tout, c'est la passion pour la vidéo et la création visuelle. Chaque projet est une nouvelle aventure où je m'engage pleinement pour garantir un résultat à la hauteur de vos attentes. Que vous ayez besoin d'un film institutionnel pour valoriser votre entreprise, d'un teaser percutant pour promouvoir un événement. Mon objectif est de donner vie à vos idées, de capturer et de transmettre l'émotion à travers l'image. Avec un œil attentif pour les détails et un engagement sans faille, je vous propose des réalisations de qualité qui sauront marquer les esprits.
 Merci de votre visite et au plaisir de collaborer sur vos futurs projets vidéo !
             </p>
