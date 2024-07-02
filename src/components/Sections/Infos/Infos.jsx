71fd3c75f2c242f74d7dae675724bf64f6b05456
Heighties <heighties@gmail.com>
2024-07-02 18:38:42 +0200

    responsive

--- src/components/Sections/Infos/Infos.jsx
+++ src/components/Sections/Infos/Infos.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Infos = () => {
   return (
     <div className="w-full flex p-4">
-      <div className="flex flex-col md:flex-row items-center sm:items-center md:items-stretch gap-2 md:gap-0 w-full">
+      <div className="flex flex-col md:flex-row items-center sm:items-center md:items-stretch gap-2 md:gap-0 w-full md:mt-4 lg:mt-auto">
         <div className='md:w-1/3 md:h-3/4 xl:h-full'>
         <img src={process.env.PUBLIC_URL + "/photoP2.jpg"} alt="Profile" className="w-40 h-40 md:w-full md:h-full object-cover object-top xl:object-center" />
         </div>
@@ -14,7 +14,7 @@ const Infos = () => {
           </h1>
           <div className="flex flex-col gap-4 items-center flex-grow p-4 md:p-0">
             <div className="w-full h-1 bg-black"></div>
-            <p className="text-justify ml-4 text-xs md:text-xs xl:text-md">
+            <p className="text-justify ml-4 text-xs md:text-xs xl:text-lg">
             Je m'appelle Nima GHASHGHAEI, et je suis vidéaste passionné. Fort d'une formation spécialisée dans le domaine du documentaire, j'ai eu la chance de travailler sur une grande variété de projets et de toucher à différents genres cinématographiques. Mon parcours m'a permis d'explorer des domaines aussi divers que le film institutionnel, les teasers, le film documentaire, et même la fiction.  Ce qui m'anime avant tout, c'est la passion pour la vidéo et la création visuelle. Chaque projet est une nouvelle aventure où je m'engage pleinement pour garantir un résultat à la hauteur de vos attentes. Que vous ayez besoin d'un film institutionnel pour valoriser votre entreprise, d'un teaser percutant pour promouvoir un événement. Mon objectif est de donner vie à vos idées, de capturer et de transmettre l'émotion à travers l'image. Avec un œil attentif pour les détails et un engagement sans faille, je vous propose des réalisations de qualité qui sauront marquer les esprits.
 Merci de votre visite et au plaisir de collaborer sur vos futurs projets vidéo !
             </p>
