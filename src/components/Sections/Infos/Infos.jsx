e38cc63f08272b93ca642d2430bd5ed1e39ef86c
Heighties <heighties@gmail.com>
2024-07-02 18:26:45 +0200

    responsive

--- src/components/Sections/Infos/Infos.jsx
+++ src/components/Sections/Infos/Infos.jsx
@@ -18,7 +18,7 @@ const Infos = () => {
             Je m'appelle Nima GHASHGHAEI, et je suis vidéaste passionné. Fort d'une formation spécialisée dans le domaine du documentaire, j'ai eu la chance de travailler sur une grande variété de projets et de toucher à différents genres cinématographiques. Mon parcours m'a permis d'explorer des domaines aussi divers que le film institutionnel, les teasers, le film documentaire, et même la fiction.  Ce qui m'anime avant tout, c'est la passion pour la vidéo et la création visuelle. Chaque projet est une nouvelle aventure où je m'engage pleinement pour garantir un résultat à la hauteur de vos attentes. Que vous ayez besoin d'un film institutionnel pour valoriser votre entreprise, d'un teaser percutant pour promouvoir un événement. Mon objectif est de donner vie à vos idées, de capturer et de transmettre l'émotion à travers l'image. Avec un œil attentif pour les détails et un engagement sans faille, je vous propose des réalisations de qualité qui sauront marquer les esprits.
 Merci de votre visite et au plaisir de collaborer sur vos futurs projets vidéo !
             </p>
-            <div className="mt-auto mb-4">
+            <div className="mt-auto md:-mt-0 xl:mt-auto mb-4 md:mb-0 xl:mb-4">
               <button className="bg-black text-white rounded-3xl w-20 h-6 text-sm xl:text-base xl:w-40 xl:h-10">Mon CV</button>
             </div>
           </div>
